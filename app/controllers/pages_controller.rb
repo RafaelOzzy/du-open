e761d77b5532663dd151123d405044f8c06d1438
Rafael Santos <rafael.ramos.s@gmail.com>
2024-03-20 22:09:51 -0300

    Landing page plus other things
    landing page, forms, user page, edit user and forget password

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  # skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @user = current_user
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :surname, :email)
   end
 end
